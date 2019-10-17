7a5a205a1185f7eb47f1c6cd10ede083da7202b1
guox <guox@goodrain.com>
2019-10-17 12:24:58 +0800

    init
    init for dockerfile

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM microsoft/dotnet:2.2-sdk-alpine AS Builder
+WORKDIR /app
+COPY . .
+RUN ${DOTNET_RESTORE_PRE} && dotnet restore && dotnet publish -c Release -o /out
+CMD ["cp","-r","/out/","/tmp/"]
+
+FROM microsoft/dotnet:2.2-aspnetcore-runtime-alpine
+
+WORKDIR /app
+COPY --from=Builder /app .
+EXPOSE 5060
+CMD ["dotnet Sample.RabbitMQ.MySql.dll"]
--- Sample.RabbitMQ.MySql/Program.cs
+++ Sample.RabbitMQ.MySql/Program.cs
@@ -14,17 +14,11 @@ namespace Sample.RabbitMQ.MySql
     {
         public static void Main(string[] args)
         {
-            string port = "5000";
-            if (Environment.GetEnvironmentVariable("PORT") != null) {
-                port = Environment.GetEnvironmentVariable("PORT");
-            }
-            Console.WriteLine("Listing :{0}", port);
-            CreateWebHostBuilder(port, args).Build().Run();
+            CreateWebHostBuilder(args).Build().Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string port, string[] args) =>
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseUrls ($"http://0.0.0.0:{port}")
                 .UseStartup<Startup>();
     }
 }
